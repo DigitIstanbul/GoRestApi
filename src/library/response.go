1d34fef2c90901bec652c21a0b49009b578dd225
beytullah <b1c87f1224>
2020-07-26 18:55:52 +0300

    Hello World!

--- src/library/response.go
+++ src/library/response.go
@@ -7,18 +7,15 @@ import (
 )
 
 func SuccessResponse(statusCode int, message string, data interface{}) gin.H {
-
-	if statusCode == 0 {
-		statusCode = 200
-	}
 	return gin.H{"meta": gin.H{"requestId": uuid.New(), "statusCode": http.StatusOK, "message": message}, "result": data}
-
 }
+
 func ErrorResponse(statusCode int, message string) gin.H {
 
 	if statusCode == 0 {
 		statusCode = 400
 	}
-	return gin.H{"meta": gin.H{"requestId": uuid.New(), "statusCode": http.StatusOK, "message": message}, "result": gin.H{}}
+
+	return gin.H{"meta": gin.H{"requestId": uuid.New(), "statusCode": statusCode, "message": message}, "result": gin.H{}}
 
 }
